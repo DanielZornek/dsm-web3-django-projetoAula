ef5d03af5eddd245e8449df9e64cbb7df8a6b495
DanielZornek <daniel_zornek91@outlook.com>
2025-04-04 14:51:34 -0300

    criado página e url para registrar novo usuário

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.app, name="app"),
-    path('usuarios', views.exibirUsuarios, name="exibirUsuarios") # digitar usuarios para entrar na página na url
+    path('usuarios', views.exibirUsuarios, name="exibirUsuarios"), # digitar usuarios para entrar na página na url
+    path('add-usuario', views.addUsuario, name="addUsuario")
+    # rota que vai apárecer na url, view, nome
 ]
--- app/views.py
+++ app/views.py
@@ -18,3 +18,6 @@ def exibirUsuarios(request):
     return render(request, "usuarios.html", 
                   {'listaUsuarios': usuarios}) # passando os usuarios como parametro para mostrar eles na pagina
                     # {} PARA PASSAR PARA DICIONARIO PQ VEM COMO ARRAY ASSOCIATIVO
+
+def addUsuario(request):
+    return render(request, "add-usuario.html")
